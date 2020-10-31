a9eeba50b0faf91052f5b60e384bba100986bc2f
Adam Chyciński <24207194+Seseikelele@users.noreply.github.com>
2020-10-31 10:45:41 +0100

    Zadanko 2

---
+++ Lab01/HenloWorld/HenloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HenloWorld
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://www.pja.edu.pl"/*args[0]*/);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var re = new Regex("[a-z0-9]+@[a-z.]+");
+
+                MatchCollection matches = re.Matches(html);
+                foreach(var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+    }
+}
